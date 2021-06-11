6165f0295f1669b32ec51364fd2fa4550da51d75
sherwin7 <sherwin@karunya.edu.in>
2021-06-11 18:36:20 +0530

    added navigation component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* navbar */}
+      <NavBar />
       {/* results */}
     </div>
   );
