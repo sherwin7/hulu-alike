2b2e6ed20f07450c52b051fbd3505a39bcd8a837
sherwin7 <sherwin@karunya.edu.in>
2021-06-12 20:38:18 +0530

    final build

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ results }) {
   return (
     <div>
       <Head>
-        <title>Hulu Clone</title>
+        <title>Hulu Alike</title>
         <meta name="description" content="A streaming platform" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
